708f5c9892ef089e891a49f264e6a3190785c81c
WHM0304 <whm0304@naver.com>
2024-01-09 23:33:13 +0900

    달력 JS

--- calendar.js
+++ calendar.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const main_nav = document.querySelector("body");
-
+  let month_num = 0;
   main_nav.addEventListener("click", (event) => {
     const main_name = event.target.tagName;
     const nav_class = event.target.className;
@@ -15,6 +15,42 @@ document.addEventListener("DOMContentLoaded", () => {
       } else if (nav_class === "fa-solid fa-bell") {
         document.location.href = "./bell.html";
       }
+    }
+  });
+
+  const date = new Date();
+  document.getElementById("year").innerHTML = date
+    .toDateString()
+    .substring(10);
+  document.getElementById("month").innerHTML = date
+    .toDateString()
+    .substring(4, 7);
+
+  const month = document.querySelector("div.month");
+  const month_prev = document.querySelector("li.prev");
+  const month_next = document.querySelector("li.next");
+  month.addEventListener("click", (event) => {
+    const target = event.target;
+    const tag_class = event.target.className;
+
+    if (tag_class === "next") {
+      month_num = month_num + 1;
+      const date = new Date(2024, month_num);
+      document.getElementById("year").innerHTML = date
+        .toDateString()
+        .substring(10);
+      document.getElementById("month").innerHTML = date
+        .toDateString()
+        .substring(4, 7);
+    } else if (tag_class === "prev") {
+      month_num = month_num - 1;
+      const date = new Date(2024, month_num);
+      document.getElementById("year").innerHTML = date
+        .toDateString()
+        .substring(10);
+      document.getElementById("month").innerHTML = date
+        .toDateString()
+        .substring(4, 7);
     }
   });
 });
