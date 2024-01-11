581ab40bcc39a7a74c667f29f71fc4784bc85589
n96js <n96js@naver.com>
2024-01-11 15:00:47 +0900

    수정

--- js/index6.js
+++ js/index6.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const freezingButton = document.querySelector(".button:nth-child(2)");
   const outdoorButton = document.querySelector(".button:nth-child(3)");
 
-  // 버튼을 클릭했을 때 배경색이 바뀌는 함수를 정의합니다.
+  // 버튼을 클릭했을 때 배경색이 바뀌는 함수를 정의
   function changeBackgroundColor(event) {
-    // 선택된 버튼의 배경색을 변경합니다.
+    // 선택된 버튼의 배경색을 변경
     event.target.style.backgroundColor = "#858585";
 
-    // 다른 버튼들의 배경색을 초기화합니다.
+    // 다른 버튼들의 배경색을 초기화
     if (event.target !== refrigerationButton) {
       refrigerationButton.style.backgroundColor = "";
     }
@@ -25,6 +25,29 @@ document.addEventListener("DOMContentLoaded", () => {
   freezingButton.addEventListener("click", changeBackgroundColor);
   outdoorButton.addEventListener("click", changeBackgroundColor);
 
+  const input_name = document.querySelector("input[name='name']");
+  const input_count = document.querySelector("input[name='count']");
+  const input_date = document.querySelector("input[name='date']");
+
   const btn_add = document.querySelector("section button.save");
-  btn_add.addEventListener;
+
+  btn_add.addEventListener("click", () => {
+    const txt_name = input_name.value;
+    const txt_count = input_count.value;
+    const txt_date = input_date.value;
+    if (!txt_name) {
+      alert("물건의 이름을 입력해주세요!!");
+      return false;
+    }
+    if (!txt_count) {
+      alert("물건의 수량을 입력해주세요!!");
+      return false;
+    }
+    if (!txt_date) {
+      alert("물건의 유통기한을 입력해주세요!!");
+      return false;
+    }
+
+    alert("추가가 완료됐습니다.");
+  });
 });
